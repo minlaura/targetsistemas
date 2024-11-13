0517cc9f1569d909c173ec2e0a9db35768dbf374
Min.Laura <148968500+minlaura@users.noreply.github.com>
2024-11-13 12:20:57 -0300

    Delete LightsOnOff.java

--- LightsOnOff.java
+++
@@ -1,3 +0,0 @@
-//Ligaria dois dos três interruptores e iria checar qual das lampadas estava apagada
-//sabendo qual era, eu apagaria mais uma lâmpada e iria checar qual das outras duas que estavam ligadas foi apagada, assim,
-//ficando apenas uma lâmpada ligada, iria descobrir os interruptores de cada lâmpada.
